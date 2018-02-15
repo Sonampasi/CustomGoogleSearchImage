625da0c3258fe2412568168820b5923e8a016b81
Sonampasi <sonampasips@gmail.com>
2018-02-15 11:48:21 +0530

    changing architecture

--- app/src/main/java/com/sonam/apps/googleimagesearch/CustomApplication.java
+++ app/src/main/java/com/sonam/apps/googleimagesearch/CustomApplication.java
@@ -3,6 +3,8 @@ package com.sonam.apps.googleimagesearch;
 import android.app.Application;
 import android.content.Context;
 
+import com.squareup.leakcanary.LeakCanary;
+
 /**
  * Created by sonam on 2/12/2018.
  */
@@ -13,6 +15,12 @@ public class CustomApplication extends Application {
 
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
         CustomApplication.context = getApplicationContext();
     }
 
